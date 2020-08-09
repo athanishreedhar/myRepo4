d8055362c0361e86f5e819203920c68a18be7dd6
athanishreedhar <shridhara1213@gmail.com>
2020-08-09 23:54:44 +0530

    java shree 4th comment

--- src/TestBase1/Auto1.java
+++ src/TestBase1/Auto1.java
@@ -5,20 +5,23 @@ public class Auto1 {
 	public void method1() {
 		System.out.println("Hello java");
 	}
+
 	public void method2() {
 		System.out.println("Hello java");
 	}
+
 	public void method3() {
 		System.out.println("Hello java");
 	}
 
-
 	private void syso() {
-	
-		System.out.println("hello git world");
-		System.out.println("hello git world");
-		System.out.println("hello git world");
-		System.out.println("hello git world");
-	}
-}
 
+		System.out.println("hello git world");
+
+	}
+
+	public void method4() {
+		System.out.println("Hello java");
+	}
+
+}
