ef16ea5daf803c09a25d7617e6651c00a22b7359
athanishreedhar <shridhara1213@gmail.com>
2020-08-10 23:21:21 +0530

    QADEV001 Updated...1

---
+++ src/TestBase1/gitCode1.java
@@ -0,0 +1,19 @@
+package TestBase1;
+
+public class gitCode1 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void m1() {
+		System.out.println("hello git----1");
+		
+	}
+	
+	public void m2() {
+		System.out.println("hello git----1");
+		
+	}
+	
+}
